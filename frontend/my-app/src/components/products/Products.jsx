fc48cb9df628f990a10e81b29481df8bbd4189be
Zhenkaaf <zhenkaaf@gmail.com>
2023-05-29 18:22:55 +0300

    upd

--- frontend/my-app/src/components/products/Products.jsx
+++ frontend/my-app/src/components/products/Products.jsx
@@ -1,16 +1,26 @@
+import { useSelector } from "react-redux";
 import ProductCard from "../productCard/ProductCard";
 import styles from "./Products.module.css";
 
 const Products = () => {
+  const selectedShopId = useSelector((state) => state.shops.selectedShopId);
+  console.log(selectedShopId);
+
   return (
     <div className={styles.products}>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      {selectedShopId ? (
+        <div className={styles.products__body}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      ) : (
+        <div className={styles.empty}>Choose the shop !!!</div>
+      )}
     </div>
   );
 };
--- frontend/my-app/src/pages/home/Home.jsx
+++ frontend/my-app/src/pages/home/Home.jsx
@@ -1,12 +1,10 @@
 import Products from "../../components/products/Products";
-import Menu from "../../components/menu/Menu";
 import Sidebar from "../../components/sidebar/Sidebar";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div>
-      {/* <Menu /> */}
       <div className={styles.main}>
         <Sidebar />
         <Products />
